94e53df0325452a38cbc6a21c628b1ea396b1390
abdurafeyf <abdurafeyf@gmail.com>
2023-02-07 17:03:19 +0500

    dropdownToButtons

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-document.getElementById("submit-button").addEventListener("click", function() {
-    var dropdown = document.getElementById("dropdown");
-    var selectedValue = dropdown.options[dropdown.selectedIndex].value;
-    switch (selectedValue) {
-      case "margin":
-        window.location.href = "margin-calculator/margin.html";
-        break;
-      case "profit":
-        window.location.href = "profit-calculator/profit.html";
-        break;
-      case "position_size":
-        window.location.href = "position-size/position_size.html";
-        break;
-      case "pip_value":
-        window.location.href = "pip-value/pip_value.html";
-        break;
-      default:
-        // do nothing
-        break;
-    }
-  });
-  
+document.getElementById("margin-button").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "margin-calculator/margin.html";
+});
+
+document.getElementById("profit-button").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "profit-calculator/profit.html";
+});
+
+document.getElementById("position-size-button").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "position-size/position_size.html";
+});
+
+document.getElementById("pip-value-button").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "pip-value/pip_value.html";
+});
